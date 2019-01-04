c34f66efb1512fb324412ed7c9c057e5e5e34ffa
blood <blood@sapphire>
2019-01-04 21:06:12 +0900

    実装

--- i2cLCD.h
+++ i2cLCD.h
@@ -8,12 +8,42 @@
 #ifndef I2CLCD_H
 #define	I2CLCD_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
+//LCDコントロ－ルバイト
+typedef union {
+    uint8_t byte;
+    
+    struct {
+        unsigned RESERVE :6;
+        unsigned RS      :1;//1 data ,0 command
+        unsigned CO      :1;//1 継続コマンド 0　最後のコマンド
+    };   
+}I2CLCDControl;
 
+//テキスト出力先RAMアドレス指定制御コマンド
+typedef union {
+    uint8_t byte;
+    
+    struct {
+        unsigned DDRAMAddr:7;//DDRAM指定バイトアドレス
+        unsigned DDRAMBit :1;//DDRAM指定コマンド 1
+    };
+}DDRSetCmd;
 
+//LCDの初期化
+extern void I2CLED_Initialize(void);
 
+//ラインの描画
+// @param string 16文字までののuint8_t 配列
+// @param cnt    文字列の長さ
+// @param line   1 or 2 表示する行 0を使わないこと
+extern void writeLine(uint8_t* string,uint8_t cnt,uint8_t line);
 
 #ifdef	__cplusplus
 }
