0dbc6413dfdc656eb5b9debad1521b86bda91e9e
blood <blood@sapphire>
2019-01-04 21:05:59 +0900

    実装

---
+++ displayLCD.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   displayLCD.h
+ * Author: blood
+ *
+ * Created on January 4, 2019, 7:55 PM
+ */
+
+#ifndef DISPLAYLCD_H
+#define	DISPLAYLCD_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+//LCDへのパラメーターの表示
+extern void displayLCD(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DISPLAYLCD_H */
+
