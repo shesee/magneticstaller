073c447b97cbb911f873b6a4da50fc87e90632a1
blood <blood@sapphire>
2019-01-05 20:53:13 +0900

    リファクタリングとコメント

--- displayLCD.h
+++ displayLCD.h
@@ -16,10 +16,28 @@
 extern "C" {
 #endif
 
-//LCDへのパラメーターの表示
+//モータードライバの障害別の列挙型
+enum{
+    OCP,//過電流
+    UVLO,//低電圧
+    OTS,//過熱
+    ILIMIT//電流制限
+};   
+    
+//LCDへのモーター回転数 PWM値　ヒーター温度 PWM値の表示
 extern void displayLCD(void);
 
+//LCDへのデバッグ出力　1行目に先頭から出力される
+// @param str 出力する文字列
+// @param cnt 文字列の長さ
+extern void debugWrite(const char* str,uint8_t cnt);
 
+//LCDへの数値のデバッグ出力　1行目に先頭から出力される
+// @param val 出力する8bitの値　キャストしてね
+extern void debugWriteVal(uint8_t val);
+
+//LED表示の初期化　バッファの初期化
+extern void displayLCD_Initialize(void);
 #ifdef	__cplusplus
 }
 #endif
