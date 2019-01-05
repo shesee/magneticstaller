fc996e5a758811f2b2df3d74359b2eb832d16c50
blood <blood@sapphire>
2019-01-05 20:56:33 +0900

    コメント付け sMotor変数の移動

--- Varient.h
+++ Varient.h
@@ -16,18 +16,16 @@
 extern "C" {
 #endif
     
-#define iPWMMotorMax  63 //ロータリーエンコーダー値 MAX モーター
-#define iPWMMotorMin  6  //ロータリーエンコーダー値 MIN モーター
+#define PWMMotorMax  63 //ロータリーエンコーダー値 MAX モーター
+#define PWMMotorMin  6  //ロータリーエンコーダー値 MIN モーター
 
-#define iPWMHeaterMax  63 //ロータリーエンコーダー値 MAX ヒーター　
-#define iPWMHeaterMin  0  //ロータリーエンコーダー値 MIN ヒーター
+#define PWMHeaterMax  63 //ロータリーエンコーダー値 MAX ヒーター　
+#define PWMHeaterMin  0  //ロータリーエンコーダー値 MIN ヒーター
 
 #define SLAVE_I2C_GENERIC_RETRY_MAX     10//I2C送受信リトライ回数
     
 extern volatile int16_t iPWMMotor; //モーターのPWM値 8bit
 extern volatile int16_t iPWMHeater; //ヒーターのPWM値 6bit 0-64
-
-extern volatile uint8_t sMotor;//モーターの状態
 
 extern volatile int8_t  iHeaterTemp; //ヒーター温度
 extern volatile uint8_t iMotorRotateCnt; //ホール素子カウント
