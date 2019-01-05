c5890f0fa8ec37d5f1dc5ac22d01265ba6d5b763
blood <blood@sapphire>
2019-01-05 20:53:53 +0900

    リファクタリングとコメント sMotor変数の変更

--- i2cGeneric.h
+++ i2cGeneric.h
@@ -16,9 +16,16 @@
 extern "C" {
 #endif
     
-
-extern void i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data);//レジスタ型のデバイスに書き込む
-extern bool i2cRegisterRead1Byte(uint16_t slaveaddr, uint8_t regaddr, uint8_t* pData);//レジスタ型のデバイスから読みこむ
+//レジスタ型のI2Cデバイスに1バイト書き込む
+// @param slaveaddr I2Cスレーブアドレス 7bit 型は16bitだけど
+// @param regaddr   I2Cデバイスのレジスタのアドレスデーター　1byte目のデータ
+// @param data      I2Cデバイスのレジスタに書き込む内容 2byte目のデータ
+extern void i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data);
+//レジスタ型のデバイスから読みこむ
+// @param slaveaddr I2Cスレーブアドレス 7bit 型は16bitだけど
+// @param regaddr   I2Cデバイスのレジスタのアドレスデーター　こちらから送信する1byte
+// @param pData     こちらの読み込み要求に対してスレーブが送ってくるデーター 1byteとは限らない。
+extern bool i2cRegisterRead1Byte(uint16_t slaveaddr, uint8_t regaddr, uint8_t* pData);
 
 #ifdef	__cplusplus
 }
