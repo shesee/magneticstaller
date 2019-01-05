8430d6e3c0de619c2e2858db44315076837b6512
blood <blood@sapphire>
2019-01-05 21:00:15 +0900

    ｓMotor変数の移動

--- Varient.c
+++ Varient.c
@@ -1,10 +1,8 @@
 #include "Varient.h"
 
-volatile int16_t iPWMMotor = iPWMMotorMin; //モーターのPWM値 8bit
-volatile int16_t iPWMHeater = iPWMHeaterMin; //ヒーターのPWM値 6bit 0-64
+volatile int16_t iPWMMotor = PWMMotorMin; //モーターのPWM値 8bit
+volatile int16_t iPWMHeater = PWMHeaterMin; //ヒーターのPWM値 6bit 0-64
 
 volatile int8_t  iHeaterTemp = 0;; //ヒーター温度
 volatile uint8_t iMotorRotateCnt = 0; //ホール素子カウント
 volatile uint16_t iMotorRotate = 0; //ローター回転数RPM
-
-volatile uint8_t sMotor = 0;
