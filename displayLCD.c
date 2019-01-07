f41086d87e2456691847be3ac8b4774d77547222
blood <blood@sapphire>
2019-01-07 14:10:15 +0900

    Doxygenコメントの追加

--- displayLCD.c
+++ displayLCD.c
@@ -2,20 +2,25 @@
 #include "i2cLCD.h"
 #include "i2cMotor.h"
 #include "displayLCD.h"
-//モーター障害時の表示文字列
+//!モーター障害時の表示文字列
 const char* mtss[] =       {" OverVoltage ",
                             " LowVoltage  ",
                             " OverHeat    ",
                             " LimitPower  "
                             };
+
                         //0123456789ABCDEF
+//1ラインバッファ1のテンプレート
 const char* line1temp =  "MTR    RPM/     ";
+//!ラインバッファ2のテンプレート
 const char* line2temp =  "HTR    C  /     ";
 
-static char line1[16];//ライン1のバッファ
-static char line2[16];//ライン2のバッファ
+//!ライン1のバッファ
+static char line1[16];
+//ライン2のバッファ
+static char line2[16];
 
-
+// @brief ラインバッファにテンプレートをコピーする。
 void displayLCD_Initialize(void){
     for(int i=0;i<16;i++){
         line1[i] = line1temp[i];
@@ -25,7 +30,7 @@ void displayLCD_Initialize(void){
 
 // @brief 10進数をラインバッファに出力する
 // @param val 出力する8bitの数値
-// @param buff 出力先のバッファ 3個の文字列を上書きする
+// @param buff 出力先のバッファ 3個の文字列を上書きする 0サプレス
 void putd(uint8_t val,char* buff){
     int i = 3;
     while(i != 0){
@@ -34,6 +39,7 @@ void putd(uint8_t val,char* buff){
         val = val / 10 ;
     };
 };
+
 // @brief 文字列をラインバッファに出力する
 // @param str 出力する文字列
 // @param cnt 文字列の長さ
@@ -46,25 +52,29 @@ void putc(const char* str,uint8_t cnt ,char* buff){
 };
 
 void displayLCD(void){
+    // @brief モーターFAULT以外の場合は回転数とPWM出力を表示する
     if(!sMotor.byte){
         putd(iMotorRotate,line1+3);
         putd(iPWMMotor,line1+13);
     }else{
+        //モーターFAULTの原因をLCDに出力する
         if(sMotor.OCP){
-            putc(mtss[OCP],12,line1+3);
+            putc(mtss[OCP],13,line1+3);
         }else if(sMotor.UVLO){
-            putc(mtss[UVLO],12,line1+3);
+            putc(mtss[UVLO],13,line1+3);
         }else if(sMotor.OTS){
-            putc(mtss[OTS],12,line1+3);
-        }else if(sMotor.UVLO){
-            putc(mtss[ILIMIT],12,line1+3);
+            putc(mtss[OTS],13,line1+3);
+        }else if(sMotor.ILIMIT){
+            putc(mtss[ILIMIT],13,line1+3);
         }       
     }
+    // @briefヒーターの温度とPWM状態を出力する
     putd(iHeaterTemp,line2+3);
     putd(iPWMHeater,line2+13);    
-    
+    // @brief LCDにラインバッファを出力する
     writeLine((uint8_t*)line1,16,1);
     writeLine((uint8_t*)line2,16,2);
+    
 };
 
 void debugWrite(const char* str,uint8_t cnt){
