99275ab0a6bfffdabbd087b9823f8e3b54df3ad1
blood <blood@sapphire>
2019-01-07 14:07:28 +0900

    低レベル関数のextern化

--- i2cGeneric.c
+++ i2cGeneric.c
@@ -1,8 +1,7 @@
-#include "mcc_generated_files/i2c.h"
 #include "Varient.h"
 #include "i2cGeneric.h"
 
-void i2cWrite(uint16_t slaveaddr, uint8_t count, uint8_t* pData, I2C_MESSAGE_STATUS* status){
+void i2cWrite(uint16_t slaveaddr, uint8_t* pData, uint8_t count, I2C_MESSAGE_STATUS* status){
 
     *status = I2C_MESSAGE_PENDING;
 
@@ -26,7 +25,7 @@ void i2cWrite(uint16_t slaveaddr, uint8_t count, uint8_t* pData, I2C_MESSAGE_STA
 }
 
 
-void i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data){
+bool i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data){
      
     uint8_t writeBuffer[2];
     I2C_MESSAGE_STATUS status;
@@ -34,11 +33,13 @@ void i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data){
     writeBuffer[0] = (regaddr);
     writeBuffer[1] = (data);
     
-    i2cWrite(slaveaddr,2,writeBuffer,&status);
+    i2cWrite(slaveaddr, writeBuffer, 2, &status);
+    
+    return (status != I2C_MESSAGE_FAIL);
 
 }
 
-void i2cRead(uint16_t slaveaddr, uint8_t wcount,uint8_t* pwData,uint8_t icount,uint8_t* pData, I2C_MESSAGE_STATUS* status){
+void i2cRead(uint16_t slaveaddr, uint8_t* pwData, uint8_t wcount, uint8_t* pData, uint8_t icount, I2C_MESSAGE_STATUS* status){
     
     *status = I2C_MESSAGE_PENDING;
     uint8_t timeOut = 0;
@@ -86,7 +87,7 @@ bool i2cRegisterRead1Byte(uint16_t slaveaddr, uint8_t regaddr, uint8_t* pData){
     
     writeBuffer[0] = regaddr;
     
-    i2cRead(slaveaddr,1,writeBuffer,1,pData, &status);
+    i2cRead(slaveaddr, writeBuffer, 1, pData, 1, &status);
 
     return (status != I2C_MESSAGE_FAIL);
 }
--- i2cGeneric.h
+++ i2cGeneric.h
@@ -11,21 +11,40 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include "mcc_generated_files/i2c.h"
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
     
+// @bieif I2Cデバイスにブロック形で書き込む
+// @param slaveaddr I2Cスレーブアドレス 7bit
+// @param pData     デバイスに書き込むデータ
+// @param count     データ長
+// @param status    結果
+extern void i2cWrite(uint16_t slaveaddr, uint8_t* pData, uint8_t count, I2C_MESSAGE_STATUS* status);
+
+// brief I2Cデバイスからブロック形で読みこむ
+// @param slaveaddr I2Cスレーブアドレス 7bit
+// @param pwData    デバイスに書き込むデータ
+// @param wcount    書き込むデータ長
+// @param pData     I2Dデバイスから受信したデータ
+// @param count     受信するデータ長 際限なくデータを送ってくるデバイスがあるのでNACKで強制的に受信を終了する。
+// @param status    結果
+extern void i2cRead(uint16_t slaveaddr, uint8_t* pwData, uint8_t wcount, uint8_t* pData, uint8_t icount, I2C_MESSAGE_STATUS* status);
+
 // @brief レジスタ型のI2Cデバイスに1バイト書き込む
 // @param slaveaddr I2Cスレーブアドレス 7bit 型は16bitだけど
 // @param regaddr   I2Cデバイスのレジスタのアドレスデーター　1byte目のデータ
 // @param data      I2Cデバイスのレジスタに書き込む内容 2byte目のデータ
-extern void i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data);
+// @return          成功したか否か
+extern bool i2cRegisterWrite1Byte(uint16_t slaveaddr, uint8_t regaddr ,uint8_t data);
 
 // @brief レジスタ型のデバイスから読みこむ
 // @param slaveaddr I2Cスレーブアドレス 7bit 型は16bitだけど
 // @param regaddr   I2Cデバイスのレジスタのアドレスデーター　こちらから送信する1byte
 // @param pData     こちらの読み込み要求に対してスレーブが送ってくるデーター 1byteとは限らない。
+// @return          成功したか否か
 extern bool i2cRegisterRead1Byte(uint16_t slaveaddr, uint8_t regaddr, uint8_t* pData);
 
 #ifdef	__cplusplus
