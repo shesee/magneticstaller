08e182d181a0779637a498f6d2e779d93e4e05be
blood <blood@sapphire>
2019-01-08 13:08:04 +0900

    以前値を保存するようにして不要なI2Cバス駆動をやめる

--- Varient.c
+++ Varient.c
@@ -1,8 +1,17 @@
 #include "Varient.h"
 
-volatile int16_t iPWMMotor = PWMMotorMin; //モーターのPWM値 8bit
-volatile int16_t iPWMHeater = PWMHeaterMin; //ヒーターのPWM値 6bit 0-64
+volatile int16_t iPWMMotor = PWMMotorMin; 
+volatile int16_t iPWMHeater = PWMHeaterMin;
 
-volatile int8_t  iHeaterTemp = 0;; //ヒーター温度
-volatile uint8_t iMotorRotateCnt = 0; //ホール素子カウント
-volatile uint16_t iMotorRotate = 0; //ローター回転数RPM
+volatile int8_t  iHeaterTemp = 0;;
+volatile uint8_t iMotorRotateCnt = 0; 
+volatile uint16_t iMotorRotate = 0;
+
+
+volatile int16_t iPrevPWMMotor = 0;
+
+volatile int16_t iPrevPWMHeater = 0; 
+
+volatile int8_t  iPrevHeaterTemp = 0;
+
+volatile uint16_t iPrevMotorRotate = 0; 
